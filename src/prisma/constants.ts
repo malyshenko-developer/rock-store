a17994a88e658669f3b394c5663cca098e2396b5
malyshenko-developer <nikitamalyshenkodeveloper@gmail.com>
2025-01-17 10:59:07 +1000

    main: Added Seeding by Prisma, Added api for search products

---
+++ src/prisma/constants.ts
@@ -0,0 +1,83 @@
+export const CATEGORIES = [
+    {
+        name: 'Гитары',
+    },
+    {
+        name: 'Клавишные инструменты',
+    },
+    {
+        name: 'Укулеле',
+    },
+    {
+        name: 'Ударные инструменты',
+    },
+    {
+        name: 'Микрофоны и радиосистемы',
+    },
+    {
+        name: 'Наушники и гарнитуры',
+    }
+];
+
+export const PRODUCTS = [
+    {
+        name: 'IBANEZ GRG7221QA-TKS',
+        price: 51490,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/9mm/wwh/utf/hoo/sg8/ogk/k0c/kog/9mmwwhutfhoosg8ogkk0ckog0.jpg',
+        categoryId: 1,
+      },
+      {
+        name: 'IBANEZ GRGR131EX-BKF',
+        price: 34010,
+        imageUrl: 'https://www.muztorg.ru/files/7vq/y7t/8pk/ccg/ssw/0os/os8/ogc/g/7vqy7t8pkccgssw0osos8ogcg.jpg',
+        categoryId: 1,
+      },
+      {
+        name: 'IBANEZ GRGR221PA-AQB',
+        price: 42560,
+        imageUrl: 'https://www.muztorg.ru/files/8ly/qa7/9yh/ao8/wcc/sc4/s8g/kwk/8lyqa79yhao8wccsc4s8gkwk0.jpg',
+        categoryId: 1,
+      },
+      {
+        name: 'ROCKDALE Stars Plus HT HH Black',
+        price: 14300,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/dfu/811/mqs/rw4/k00/88w/4gs/sww/8/dfu811mqsrw4k0088w4gssww8.jpg',
+        categoryId: 1,
+      },
+      {
+        name: 'IBANEZ GRG121SP-GYC',
+        price: 45190,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/2mh/l70/l23/bsw/s8w/kwk/c00/4ss/c/2mhl70l23bsws8wkwkc004ssc.jpg',
+        categoryId: 1,
+      },
+      {
+        name: 'IBANEZ GRG121DX-BKF',
+        price: 33110,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/5zk/jmc/q7y/18o/ccs/00s/kks/cgw/g/5zkjmcq7y18occs00skkscgwg.jpg',
+        categoryId: 1,
+      },
+      {
+        name: 'KORG LP-380 WH U',
+        price: 114990,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/c3v/9cr/m36/5ck/sg0/cs0/ssw/s4s/s/c3v9crm365cksg0cs0ssws4ss.jpg',
+        categoryId: 2,
+      },
+      {
+        name: 'Artesia Performer Black',
+        price: 26970,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/73r/lo4/fhs/fwg/sgo/0s0/kws/040/8/73rlo4fhsfwgsgo0s0kws0408.jpg',
+        categoryId: 2,
+      },
+      {
+        name: 'KORG LP-380 RW U',
+        price: 112490,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/8y6/wfs/ogo/2sk/8gw/ck8/cww/ogw/8y6wfsogo2sk8gwck8cwwogw0.jpg',
+        categoryId: 2,
+      },
+      {
+        name: 'YAMAHA PSR-SX900, без адаптера',
+        price: 279990,
+        imageUrl: 'https://www.muztorg.ru/files/sized/f250x250/6du/gxn/6ej/b0g/s88/0ok/soo/s0g/c/6dugxn6ejb0gs880oksoos0gc.jpeg',
+        categoryId: 2,
+      }
+]
---
+++ src/prisma/seed.ts
@@ -0,0 +1,90 @@
+import { CATEGORIES, PRODUCTS } from "./constants";
+import { prisma } from "./prisma-client";
+import { hashSync } from "bcrypt";
+
+async function up() {
+    await prisma.user.createMany({
+        data: [
+            {
+                fullName: 'Тестовый юзер',
+                email: 'user@test.com',
+                password: hashSync('1234', 10),
+                verifed: new Date(),
+                role: 'USER'
+            },
+            {
+                fullName: 'Тестовый админ',
+                email: 'admin@test.com',
+                password: hashSync('5678', 10),
+                verifed: new Date(),
+                role: 'ADMIN'
+            }
+        ]
+    });
+
+    await prisma.category.createMany({
+        data: CATEGORIES
+    });
+
+    await prisma.product.createMany({
+        data: PRODUCTS
+    });
+
+    await prisma.cart.createMany({
+        data: [
+            {
+                userId: 1,
+                totalAmount: 0,
+                token: '1234'
+            },
+            {
+                userId: 2,
+                totalAmount: 0,
+                token: '5678'
+            },
+        ]
+    });
+
+    await prisma.cartItem.createMany({
+        data: [
+            {
+                productId: 1,
+                cartId: 1,
+                quantity: 2,
+            },
+            {
+                productId: 9,
+                cartId: 2,
+            },
+        ]
+    })
+}
+
+async function down() {
+    await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+    await prisma.$executeRaw`TRUNCATE TABLE "Category" RESTART IDENTITY CASCADE`;
+    await prisma.$executeRaw`TRUNCATE TABLE "Product" RESTART IDENTITY CASCADE`;
+    await prisma.$executeRaw`TRUNCATE TABLE "Cart" RESTART IDENTITY CASCADE`;
+    await prisma.$executeRaw`TRUNCATE TABLE "CartItem" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+    try {
+        await down();
+        await up();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (e) => {
+        console.error(e);
+
+        await prisma.$disconnect();
+
+        process.exit(1);
+    })
